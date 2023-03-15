e6f1316a7d7b1f68c0bd1cfd56d84fb01d0d1ee0
Vladislav Crucerescu <vcrucerescu6@gmail.com>
2023-03-15 15:48:09 +0200

    token2+example

--- Token.py
+++ Token.py
@@ -43,6 +43,58 @@ P = {
 }
 '''
 
+
+'''
+place p1, p2;
+tran t1, t2;
+p1.amm = 3;
+p2.cap = 4;
+t1.out = {p1 : 2, p2}
+p1.out = {t2 : 5}
+
+->
+
+<datatype : name="place"> <var : name="p1">, <var : name="p2">;
+<datatype : name="tran">  <var : name="t1">, <var : name="t2">;
+<var : name="p1">.amm = <number: val=3>;
+<var : name="p2">.cap = <number: val=4>;
+<var : name="t1">.out = {<arc : destination="p1", val=2>, <arc : destination="p2", val=1>}
+<var : name="p1">.out = {<arc : destination="t2", val=5>}
+
+->
+
+<datatype : name="place"> <varlist : names=["p1","p2"]>;
+<datatype : name="tran">  <varlist : names=["t1","t2"]>;
+<var : name="p1">.amm = <number: val=3>;
+<var : name="p2">.cap = <number: val=4>;
+<var : name="t1">.out = <arclist : arcs = [Arc(destination="p1",val=3), Arc(destination="p2",val=1)]>
+<var : name="p1">.out = <arclist : arcs = [Arc(destination="t2",val=5)]>
+
+->
+
+<instantiation : type="place", names=["p1","p2"]>;
+<instantiation : type="tran", names=["t1","t2"]>;
+<placefield : type="amm", name="p1", val=3>;
+<placefield : type="cap", name="p2", val=4>;
+<arcing : type="outbound", arcs = [Arc(source="t1",destination="p1",val=3), Arc(source="t1",destination="p2",val=1)]>;
+<arcing : type="inbound", arcs = [Arc(source="p1",destination="t2",val=5)]>;
+
+->
+
+<declaration : object = Instantiation(type="place", names=["p1","p2"])>
+<declaration : object = Instantiation(type="tran", names=["t1","t2"])>
+<declaration : object = Placefield(type="amm", name="p1", val=3)>
+<declaration : object = Placefield(type="cap", name="p2", val=4)>
+<declaration : object = Arcing(type="outbound", arcs = [Arc(source="t1",destination="p1",val=3), Arc(source="t1",destination="p2",val=1))>
+<declaration : object = Arcing(type="inbound", arcs = [Arc(source="p1",destination="t2",val=5))>
+'''
+
+'''
+Methods
+1. separate each word in a list
+2. tokenize
+3. merge tokens into declarations
+'''
 DIGITS = '0123456789'
 LETTERS = string.ascii_letters
 CHARS = LETTERS + DIGITS + '_'
@@ -53,7 +105,6 @@ KEYWORDS = [
     'amm',
     'cap'
 ]
-
 
 # TOKENS
 
@@ -66,3 +117,11 @@ TT_DOT = 'DOT'
 TT_EQUAL = 'EQUAL'
 TT_COLON = 'COLON'
 TT_SEMICOLON = 'SEMICOLON'
+
+def declaration_splitter(code_string: str):
+    declarations = code_string.replace('\n','').split(';')[:-1]
+    for declaration in declarations:
+        print(declaration)
+
+example = open("petrinets/example.txt", 'r')
+declaration_splitter(example.read())
